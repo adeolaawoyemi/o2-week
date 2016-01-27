d81ef4d7ceba16c2a9b63d066a83f1e7f2268012
Adeola Awoyemi <adeola.awoyemi@teamaol.com>
2016-01-27 00:37:49 +0000

    fix bug with getting week number

--- background.js
+++ background.js
@@ -1,15 +1,28 @@
-Date.prototype.getWeekNumber = function(){
-    var d = new Date(+this);
-    d.setHours(0,0,0);
-    d.setDate(d.getDate()+4-(d.getDay()||7));
-    return Math.ceil((((d-new Date(d.getFullYear(),0,1))/8.64e7)+1)/7);
+function ISO8601_week_no(dt) {
+    var tdt = new Date(dt.valueOf());
+    var dayn = (dt.getDay() + 6) % 7;
+    tdt.setDate(tdt.getDate() - dayn + 3);
+    var firstThursday = tdt.valueOf();
+    tdt.setMonth(0, 1);
+    if (tdt.getDay() !== 4) {
+        tdt.setMonth(0, 1 + ((4 - tdt.getDay()) + 7) % 7);
+    }
+    return 1 + Math.ceil((firstThursday - tdt) / 604800000);
+}
+
+var padNumber = function(num) {
+    return num < 10 ? '0' + num : num;
 };
 
-(function updateSprintLabel() {
-  var today = new Date();
-  var year = String(today.getFullYear()).substring(2);
-  var week = today.getWeekNumber();
-  var sprint = year + '.' + week;
-  chrome.browserAction.setBadgeText({text: sprint});
-  setTimeout(updateSprintLabel, 60*60*1000);
+;(function() {
+    document.addEventListener('DOMContentLoaded', function() {
+        var container = document.querySelector('#week');
+        var today = new Date();
+        var year = String(today.getFullYear()).substring(2);
+        var week = ISO8601_week_no(today);
+        var sprint = year + '.' + padNumber(week);
+        console.log('sprint:', sprint);
+        if (container) container.innerHTML = sprint;
+        chrome.browserAction.setBadgeText({text: sprint});
+    }, false);
 })();
